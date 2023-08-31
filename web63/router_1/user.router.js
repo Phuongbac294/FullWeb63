18a9962484b33118f174ea62a154e42f7412b611
Phuongbac294 <110046513+Phuongbac294@users.noreply.github.com>
2023-08-31 17:40:14 +0700

    1

--- web63/router_1/user.router.js
+++ web63/router_1/user.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const MongoClient = require('mongodb').MongoClient;
 
 const praviter_Key = "galkghakfdklgrgldkaal"
 
@@ -12,12 +13,36 @@ router.get('/', (req, res) => {
 
 router.post('/login', (req, res) => {
     let {username, password} = req.body;
-    console.log(username, password);
-    if (!username || !password) { res.json({sucess: false, token: ""}) }
+    if (!username || !password) { res.json({sucess: false, token: ""}) } // ktra username, password nhap khong dung cu phat
     else {
-        let token = jwt.sign({username: username}, praviter_Key)
-        res.json({username: username, token: token})
+        MongoClient.connect("mongodb+srv://Web63_atlas:Web63atlas@cluster0.wntgsqq.mongodb.net/", (err, client)   => {
+            if (err)  throw err; //truyền dữ liệu vào mongodb, ktra dữ liệu
+            const db = client.db("Web63"); // vào dữ liệu database
+
+            db.collection("users") // vào file collection
+                .find({ username: username, password: password }) // tìm kiểm document trong collection
+                .toArray((err, result) => {
+                    if (err) throw err;
+                    if (result && result != null && result.length>0) {
+                        let token = jwt.sign({username: username, email: result[0].email}, praviter_Key)
+                        res.json({username: username, token: token})
+                        console.log(result);
+                    } else {
+                        res.json({sucess: false, token: ""})
+                    }
+                })
+        })
     }
 });
+
+router.put('/add', (req, res) => {
+    MongoClient.connect('mongodb+srv://Web63_atlas:Web63atlas@cluster0.wntgsqq.mongodb.net/')
+    async (err, client) => {
+        if (err) {res.send("Couldn't connect to Mongo")}
+        const db = client.db('Web63');
+        const result = await db.collection('users').insertOne(req.body)
+        res.send(result);
+    }
+})
 
 module.exports = router;
